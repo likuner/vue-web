b97fa15cbdb7900794940aec83731b8bb0f29f26
likun <273676636@qq.com>
2023-03-01 13:24:41 +0800

    vue-runtime-core

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,11 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    // $log: any
+  }
+}
+
+export { }
